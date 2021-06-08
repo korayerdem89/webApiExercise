904a72b7dee61bfd7e46f9f3f3f3f7e606ad7efe
Koray Erdem <developerkoray@gmail.com>
2021-06-08 22:13:46 +0300

    webApi exercise first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+        <Text>Hello Worldies and Ladies!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
