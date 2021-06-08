208f03205828776fd80b25d88c89438753f19921
Koray Erdem <developerkoray@gmail.com>
2021-06-08 23:12:59 +0300

    then and catch exercise is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import axios from 'axios';
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
 
 const App = () => {
+  function fetchData() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(console.log('response'))
+      .catch(error => console.log('alaarm alaarm'));
+  }
   return (
     <SafeAreaView>
       <View>
         <Text>Hello World!</Text>
-        <Text>Hello Worldies and Ladies!</Text>
+        <Button title="Fetch Data" onPress={fetchData} />
       </View>
     </SafeAreaView>
   );
